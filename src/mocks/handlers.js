d73608fba74af84e3ae5f67d101708eaeacbb4db
Satya Achanta Venkata <elevator.satya@gmail.com>
2022-12-25 12:49:19 -0500

    Add ToppingOption component and update tests

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -7,4 +7,21 @@ export const handlers = [
             { name: 'Vanilla', imagePath: '/images/vanilla.png' },
         ]))
     })),
+
+    rest.get("http://localhost:3030/toppings", ((req, res, ctx) => {
+        return res(ctx.json([
+            {
+                "name": "M&Ms",
+                "imagePath": "/images/m-and-ms.png"
+            },
+            {
+                "name": "Hot fudge",
+                "imagePath": "/images/hot-fudge.png"
+            },
+            {
+                "name": "Peanut butter cups",
+                "imagePath": "/images/peanut-butter-cups.png"
+            },
+        ]));
+    })),
 ];
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -14,3 +14,22 @@ test("should display image for each scoop from server", async () => {
 
   expect(altTextContent).toEqual(["Chocolate scoop", "Vanilla scoop"]);
 });
+
+test("should display image for each topping from server", async () => {
+  render(<Options optionType="toppings" />);
+
+  // find images
+  const toppingImages = await screen.findAllByRole("img", {
+    name: /topping$/i,
+  });
+
+  expect(toppingImages).toHaveLength(3);
+
+  const altTextContent = toppingImages.map((element) => element.alt);
+
+  expect(altTextContent).toEqual([
+    "M&Ms topping",
+    "Hot fudge topping",
+    "Peanut butter cups topping",
+  ]);
+});
