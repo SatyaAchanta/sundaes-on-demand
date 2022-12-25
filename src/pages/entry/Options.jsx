f5c1e3d09fde086d8dd03c32f656fb7c47ded459
Satya Achanta Venkata <elevator.satya@gmail.com>
2022-12-25 13:26:07 -0500

    Add tests for error condition and alert component

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,23 +1,28 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "../../common/AlertBanner";
 import ScoopOption from "./ScoopOptions";
 import ToppingOption from "./ToppingOptions";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setErrorState] = useState(false);
 
   // optionType is 'scoops' or 'toppings
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: handle error response
+      .catch((_) => {
+        setErrorState(true);
       });
   }, [optionType]);
 
-  // TODO: replace `null` with ToppingOption when available
+  if (error) {
+    return <AlertBanner />;
+  }
+
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
   const optionItems = items.map((item) => (
